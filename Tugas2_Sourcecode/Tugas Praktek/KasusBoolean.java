c7415ffabc4a35c596cb8349a24ea627e481b664
aspriantiindacahyanijasmani <127595682+aspriantiindacahyanijasmani@users.noreply.github.com>
2023-03-26 19:40:36 +0800

    Add files via upload

---
+++ Tugas2_Sourcecode/Tugas Praktek/KasusBoolean.java
@@ -0,0 +1,29 @@
+/* NIM      : 13020210236
+Nama        : Asprianti Inda Cahyani J
+Hari/Tgl    : Sabtu, 25 Maret 2023
+Waktu       : 12.30  PM */
+package tgs6;
+
+/* Eksrpesi kondisional dengan boolean */
+public class KasusBoolean {
+
+    /**
+    * @param args
+    */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        /* Kamus */
+        boolean bool;
+        /* Algoritma */
+        bool = true;
+        if(bool) {
+            System.out.print("true\n");
+        } else
+            System.out.print("false\n");
+        if(!bool) {
+            System.out.print("salah\n");
+        } else
+            System.out.print("benar\n");
+
+    } 
+}
